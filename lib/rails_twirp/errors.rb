dcda7692895c3643f228e3c447315f66b6ee2894
Emil Stolarsky <emil@cheddar.me>
2021-10-15 12:57:26 -0400

    Add error logging to Twirp error handler

--- lib/rails_twirp/errors.rb
+++ lib/rails_twirp/errors.rb
@@ -4,8 +4,24 @@ module RailsTwirp
   module Errors
     # Helper that sets the response to a Twirp Error
     # The valid error codes can be found in Twirp::ERROR_CODES
-    def error(code, message, meta = nil)
+    def error(code, message, meta: nil, exception: nil)
+      if exception.present?
+        Rails.logger.error(exception.message)
+        Rails.logger.error(exception.backtrace.join("\n"))
+      end
+
+      Rails.logger.error("code=#{code} message=#{message} meta=[#{stringify_meta(meta)}]")
       self.response_body = Twirp::Error.new(code, message, meta)
+    end
+
+    private
+
+    def stringify_meta(meta)
+      return "" if meta.nil?
+
+      meta.to_a.each do |kv_pair|
+        kv_pair.join("=")
+      end.join(" ")
     end
   end
 end
