540286f0ee167a47c9cdeac20682dc881ccd9adc
allockicmoi <allockicmoi@gmail.com>
2021-11-18 11:54:27 +0000

    standardrb fix

--- test/dummy_test.rb
+++ test/dummy_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class DummyControllerTest < RailsTwirp::IntegrationTest
   test "controller gets rpc name" do
-    req = RPC::DummyAPI::RpcNameCheckRequest.new()
+    req = RPC::DummyAPI::RpcNameCheckRequest.new
     rpc RPC::DummyAPI::DummyService, "RpcNameCheck", req
     assert_instance_of RPC::DummyAPI::RpcNameCheckResponse, response
     assert_equal "RpcNameCheck", response.rpc_name
