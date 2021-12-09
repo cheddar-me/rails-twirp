61fa148e23125ce44dbc357817d8bd5069217858
Bouke van der Bijl <bouke@cheddar.me>
2021-12-09 12:01:35 +0100

    standardrb --fix

--- lib/rails_twirp/exception_handling.rb
+++ lib/rails_twirp/exception_handling.rb
@@ -16,10 +16,11 @@ module RailsTwirp
       # 2. When we want to show detailed exceptions we include the exception message in the error
       if http_request.get_header("action_dispatch.show_detailed_exceptions")
         self.response_body = Twirp::Error.internal_with(e)
-      else
-        # 3. Otherwise we just return a vague internal error message
-        self.response_body = Twirp::Error.internal("Internal error")
+        return
       end
+
+      # 3. Otherwise we just return a vague internal error message
+      self.response_body = Twirp::Error.internal("Internal error")
     end
   end
 end
